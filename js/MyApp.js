41787938b8c6d58e47156e58996b8a35b1a6ac60
Dev Nagar <devnagar@Live.com>
2015-04-06 18:20:05 +0530

    first commit

---
+++ js/MyApp.js
@@ -0,0 +1,28 @@
+﻿var myApp = angular.module('myApp', []);
+myApp.controller('PropertyController', function ($scope, $http, $filter) {
+
+    $scope.MyClick = function () {
+        $scope.Error = "";
+
+        debugger;
+        // method: type Get/Post (get for http get protocal like select operation, 
+        // and Post method type for save data and update data like this
+        //when we will use method then parameter in: Get ----->params
+        // when we will use method then parameter in: Post-----data
+        $http({
+            method: 'Get',
+            url: 'http://kusg4393.azurewebsites.net/Main/driver/GetNewOrders?driverid=D9042D3C-D356-469E-9615-23FFE86C7EFA',
+            params: { }
+        }).success(function (data, status, headers, config) {
+            debugger;
+            $scope.MyDriverList = data;
+            debugger
+        });
+    }
+
+    $scope.AcceptOrder = function () {
+        alert('hi')
+        // loop in checkbox and get all checked orders.
+       
+    }
+});
